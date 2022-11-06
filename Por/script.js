1d23293ba668fe2ccc58e1db8b6e2caeb7fa66c2
Por2002 <tanapolnimnaul@gmail.com>
2022-11-07 00:07:51 +0700

    update

--- Por/script.js
+++
@@ -1,9 +0,0 @@
-$("#carousel slidel").slick({
-    arrows: false,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    mobileFirst: true
-});
