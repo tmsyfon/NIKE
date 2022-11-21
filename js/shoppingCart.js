0b069f3231212dcedd60e902ad1757da85fb8903
tmsyfon <tanapolnimnaul@gmail.com>
2022-11-21 12:08:29 +0700

    update

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -6,6 +6,50 @@ window.addEventListener('load', (event) => {
     cart_initCart();
 });
 
+function create_cart() {
+	cart_getValue_v2();
+	let boxcart = document.getElementById("boxcart");
+	console.log(boxcart);
+	let template = "";
+	cartObj.forEach((item) => {
+		template += `<div class="row mb-4 d-flex justify-content-between align-items-center">
+                        <div class="col-md-2 col-lg-2 col-xl-2">
+                            <img src="${item.pic}" class="img-fluid rounded-3">
+                        </div>
+                        <div class="col-md-3 col-lg-3 col-xl-3">
+                            <h6 class="text-black mb-0"><b${item.name}</b></h6>
+                            <h6 class="text-muted">${item.type}</h6>
+                            <h6 class="text-muted">${item.color}</h6>
+                            <h6 class="text-muted">ไซส์ -</h6>
+                        </div>
+                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                            <button class="btn btn-link px-2" 
+                            onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
+                            cart_updateQtordInRow_v2(${cartObj.indexOf(item)},this.parentNode.querySelector('input[type=number]').value);
+                            cart_updateprice(${cartObj.indexOf(item)});}">
+                                <i class="fa-solid fa-minus"></i>
+                            </button>
+                            <input id="form1" min="0" name="quantity" value="1" type="number" class="form-control form-control-sm" />
+                            <button class="btn btn-link px-2" 
+                            onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
+                            cart_updateQtordInRow_v2(${cartObj.indexOf(item)},this.parentNode.querySelector('input[type=number]').value);
+                            cart_updateprice(${cartObj.indexOf(item)});}">
+                                <i class="fa-solid fa-plus"></i>
+                            </button>
+                        </div>
+                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                            <h6 class="mb-0" id="priceitem">${item.total}</h6>
+                        </div>
+                        <div class="col-md-1 col-lg-1 col-xl-1 text-start">
+                            <i class="fa-regular fa-trash-can" id="trash" onclick="cart_removeCartRow_v3(${cartObj.indexOf(item)})"></i>
+                        </div>
+                    </div>
+                    <hr class="my-4">`;
+	  
+	  });
+	  boxcart.innerHTML = template;
+}
+
 function create_nav_cart() {
 	cart_initCart();
 	let cartNavbar = document.getElementById("cart-navbar");
@@ -100,11 +144,70 @@ function addItemInCart(prod_id,qtord,price,name,pic,type,color) {
 
 }
 
+function cart_getValue_v2() {
+
+	cart_initCart();
+	var totRowWithVAT = 0;
+	for (var r of cartObj) // iterate all the item
+	{
+	    if (r==null) continue; // Removed ROW - ignore it
+
+	    var price = parseFloat(r["total"]);
+		
+	    totRowWithVAT = totRowWithVAT + (price);
+	}
+
+	cartValueObj = totRowWithVAT;
+	let product = document.getElementById("smallprice");
+	product.innerHTML = cartValueObj;
+	if (cartCI == 0){
+	let total = document.getElementById("sumprice");
+	total.innerHTML = cartValueObj;
+	}
+	else{
+		let total = document.getElementById("sumprice");
+	total.innerHTML = cartValueObj+100;
+	}
+
+}
+
 function cart_removeCartRow_v2(rowid) {
 	cartObj.splice(rowid, 1); // sign row as invalid
 	cartCI--;
 	setLocalData("USER_CART",JSON.stringify(cartObj));
 	setLocalData("USER_CART_CI",cartCI);
 	create_nav_cart();
+}
+
+function cart_removeCartRow_v3(rowid) {
+	cartObj.splice(rowid, 1); // sign row as invalid
+	cartCI--;
+	setLocalData("USER_CART",JSON.stringify(cartObj));
+	setLocalData("USER_CART_CI",cartCI);
+    create_cart();
+}
+
+function cart_updateQtordInRow_v2(rowIndex,newQtord) {
+   // like add item this function fail if qtord is not valid
+    	newQtord = parseFloat(newQtord);
+	if (!(newQtord > 0)) {
+             alert('insert positive value' );
+  
+	    return -1;
+	}
+	
+  	// update cart
+	    cartObj[rowIndex]["QTORD"] = newQtord;
+		cartObj[rowIndex]["total"] = newQtord*cartObj[rowIndex]["price"];
+     
+	 // update memory
+	setLocalData("USER_CART",JSON.stringify(cartObj));
+	cart_getValue_v2();
+}
+
+function cart_updateprice(rowid) {
+
+	let price_id = document.getElementById("priceitem"+rowid);
+	price_id.innerHTML = cartObj[rowid]["total"];
 	
 }
